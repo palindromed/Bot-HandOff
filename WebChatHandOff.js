dc2cb8c68db7272dd045c62dc72f3039c013c8c7
Hannah Krager <hakrager@microsoft.com>
2016-12-27 13:10:00 -0800

    move to obj

--- WebChatHandOff.js
+++ WebChatHandOff.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var builder = require('botbuilder');
 var app = express();
-var checkIn = [];
+var checkIn = {};
 
 
 
@@ -26,58 +26,20 @@ app.post('/api/messages', [connector.listen(), function (req, res, next) {
 
 // Create endpoint for agent / call center
 app.use('/agent', express.static('public'));
-// app.post('/agent/:args', function(req, res, next) {
-//     console.log('requests');
-//     // connector.listen();
-//     console.log(req);
-//     console.log(res);
-// });
+
 
 var connectorQueue = function (deets) {
-    // if deets don't match mine, return them?? then send msgs that way??
+
     console.log(deets);
-    checkIn.push(deets);
-    console.log(checkIn);
-    // if (checkIn.length >= 2) {
-    //     return checkIn[0] ? checkIn[0].user.id !== deets.user.id : checkIn[1];
-    // } else {
-    //     return null;
-    // }
-    if (deets.user.id !== 'hannah') {
-        return {
-            'id': 'ElDe6udZczB|000000000000000001',
-
-            'channelId': 'directline',
-
-            'user': { 'id': 'hannah', 'name': 'hannah' },
-
-            'conversation': { 'id': 'ElDe6udZczB' },
-
-            'bot': { 'id': 'handoffbotdev@Vyk0lb3f67A', 'name': 'HandOffBot' },
-
-            'serviceUrl': 'https://directline.botframework.com',
-
-            'useAuth': true
+    checkIn[deets.user.id] = deets;
+    for (addy in Object.keys(checkIn)) {
+        if (addy !== deets.user.id) {
+            return checkIn[addy];
         }
-
-    } else {
-        return {
-            'id': 'JAhN4ZEdMCv|000000000000000001',
-
-            'channelId': 'directline',
-
-            'user': { 'id': 'scott', 'name': 'scott' },
-
-            'conversation': { 'id': 'JAhN4ZEdMCv' },
-
-            'bot': { 'id': 'handoffbotdev@Vyk0lb3f67A', 'name': 'HandOffBot' },
-
-            'serviceUrl': 'https://directline.botframework.com',
-
-            'useAuth': true
-        }
-
     }
+
+    console.log(checkIn);
+
 }
 
 //=========================================================
@@ -88,13 +50,24 @@ var emergencies = ["Health", "Crime", "Catastrophe"];
 
 bot.dialog('/', [
     //welcome the user, ask the emergency
-    function (session) {
+    function (session, args, next) {
         var temp = connectorQueue(session.message.address);
         session.privateConversationData.connect = temp;
+        next();
 
-        session.replaceDialog('/chats');
+        // session.replaceDialog('/chats');
 
         // builder.Prompts.choice(session, "What's the emergency?", emergencies);
+    }, function (session, args, next) {
+        if (session.message.text !== 'break') {
+            bot.send(
+                new builder.Message()
+                    .text(session.message.text)
+                    .address(session.privateConversationData.connect));
+        } else {
+            session.endConversation('bye');
+        }
+
     }
     // //work with selected emergency
     // function (session, results) {
