4ea36ebdcb93ff230aba5d3325748ff807d4955a
Hannah Krager <hakrager@microsoft.com>
2016-12-28 15:04:12 -0800

    still not logging

--- middleware.js
+++ middleware.js
@@ -1,21 +1,18 @@
 module.exports = {
     addressBook: {},
 
-    incoming: function(message, args) {
-        if (message.user.id in addressBook) {
-            pass;
-        } else {
-            addressBook[message.address.user.id] = message.address
-        }
+    incoming: function (message, args) {
+        addressBook[message.address.user.id] = message.address
+
         console.log(addressBook);
         console.log('************');
         return message;
     },
-    outgoing: function(message, args) {
+    outgoing: function (message, args) {
         // console.log(args);
         // console.log('===========');
         // message.text = 'Outgoing test';
-        return message 
+        return message
 
     }
 }
