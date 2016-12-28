44d17a0cac9c944cc257426600183088549860f7
Hannah Krager <hakrager@microsoft.com>
2016-12-28 15:17:44 -0800

    ah human error

--- middleware.js
+++ middleware.js
@@ -4,13 +4,13 @@ module.exports = function() {
     var incoming = function (message, args) {
         // addressBook[message.address.user.id] = message.address
 
-        // console.log(addressBook);
+        console.log(addressBook);
         console.log('************');
         return message;
     };
     var outgoing = function (message, args) {
-        // console.log(args);
-        // console.log('===========');
+        console.log(args);
+        console.log('===========');
         // message.text = 'Outgoing test';
         return message
 
