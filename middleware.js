6bc4c1bf6aacdb3081b8481f38b7862bbb3e7e19
Hannah Krager <hakrager@microsoft.com>
2016-12-28 14:38:45 -0800

    what does in order mean

--- middleware.js
+++ middleware.js
@@ -1,13 +1,13 @@
 module.exports = {
     incoming: function(message, args) {
         console.log(args);
-        console.log(message);
+        console.log('************');
         message.text = 'This is a test';
         return message;
     },
     outgoing: function(message, args) {
         console.log(args);
-        console.log(message);
+        console.log('===========');
         message.text = 'Outgoing test';
         return message 
 
