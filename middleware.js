b7130fd0a180d42417706abc9bf44e28e43ca7e8
Hannah Krager <hakrager@microsoft.com>
2016-12-29 09:31:45 -0800

    why

--- middleware.js
+++ middleware.js
@@ -10,16 +10,16 @@ module.exports = {
         // 2 update info if found
         // make routing decision based on info -- this prob goes to sent
         // 3 if not found, create
-        var thisUser = global.agents[0] || global.users[0];
+        // var thisUser = global.agents[0] || global.users[0];
 
 
-        if (message.text && thisUser !== 'undefined') {
-            if (message.user.isStaff) {
-                global.agents[0] = new global.User(message);
-            } else {
-                global.users[0] = new global.User(message);
-            }
-        }
+        // if (message.text && thisUser !== 'undefined') {
+        //     if (message.user.isStaff) {
+        //         global.agents[0] = new global.User(message);
+        //     } else {
+        //         global.users[0] = new global.User(message);
+        //     }
+        // }
         message.text = 'Receive wins';
         console.log('agents');
         console.log(global.agents);
@@ -34,12 +34,12 @@ module.exports = {
     outgoing: function (message, args) {
         console.log(args);
         console.log('===========');
-        if (message.address.user.id === 'hannah') {
-            console.log('I am staff');
-            message.address = global.users[0].addy;
-        } else {
-            message.address = global.agents[0].addy;
-        }
+        // if (message.address.user.id === 'hannah') {
+        //     console.log('I am staff');
+        //     message.address = global.users[0].addy;
+        // } else {
+        //     message.address = global.agents[0].addy;
+        // }
         console.log(global.agents);
         console.log(global.users);
         message.text = 'send wins';
