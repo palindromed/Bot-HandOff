c5f796e48a1555acee7ed5c6a91aaf170e1bfcae
Hannah Krager <hakrager@microsoft.com>
2016-12-28 18:13:51 -0800

    check func

--- globals.js
+++ globals.js
@@ -1,11 +1,12 @@
 module.exports = function () {
     global.User = function (args) {
-        this.user = args.user.id,
-            this.address = args.address,
-            this.lastActivity = args.timestamp,
-            this.transcript = args.text,
-            this.agentId = false,
-            this.isAgent = args.user.isStaff
+        return 'here we are';
+        // this.user = args.user.id,
+        //     this.address = args.address,
+        //     this.lastActivity = args.timestamp,
+        //     this.transcript = args.text,
+        //     this.agentId = false,
+        //     this.isAgent = args.user.isStaff
 
     };
     global.userList = [];
