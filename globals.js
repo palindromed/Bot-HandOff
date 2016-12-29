88771e045fad7a7b10441ecbd52a5a060632bbd6
Hannah Krager <hakrager@microsoft.com>
2016-12-29 08:31:16 -0800

    testing address

--- globals.js
+++ globals.js
@@ -4,9 +4,17 @@ module.exports = function () {
         this.text = args.text,
         this.addy = args.address
         this.isAgent = args.user.isStaff || false,
-        this.lastActivity = args.timestamp
+        this.lastActivity = args.timestamp,
+        this.talkingToAgent = false 
     };
 
+    global.User.prototype.connectUserToAgent = function (moredata) {
+
+
+    };
+    global.User.prototype.getUser = function () {
+        
+    }
 
     global.users = {};
 
--- middleware.js
+++ middleware.js
@@ -22,15 +22,35 @@ module.exports = {
         console.log(global.agents);
         console.log(global.users);
         console.log('************');
-        // message.text = 'This is a test';
         return message;
     },
 
     outgoing: function (message, args) {
         console.log(args);
         console.log('===========');
-        message.text = 'Outgoing test';
-        return message
-    },
+        if (message.address.user.id === 'hannah') {
+            message.address = {
+                id: 'L8YqoKYYo7x|000000000000000001',
 
+                channelId: 'directline',
+
+                user: { id: 'scott', name: 'scott' },
+
+                conversation: { id: 'L8YqoKYYo7x' },
+
+                bot: { id: 'handoffbotdev@Vyk0lb3f67A', name: 'HandOffBot' },
+
+                serviceUrl: 'https://directline.botframework.com',
+
+                useAuth: true
+            }
+
+    }
+        return message
+},
+
+    connectToAgent: function (data) {
+
+
+    }
 }
