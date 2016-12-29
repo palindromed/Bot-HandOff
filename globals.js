6dee4435ca053bf86081a61e89a046d3356d974e
Hannah Krager <hakrager@microsoft.com>
2016-12-28 18:26:18 -0800

    extend globals again

--- globals.js
+++ globals.js
@@ -1,3 +1,9 @@
 module.exports = function() {
     global.addressBook = {};
+
+    global.User = function(args) {
+        this.text = args.text,
+        this.addy = args.address
+    };
+    global.userList = ['user1'];
 }
--- middleware.js
+++ middleware.js
@@ -1,26 +1,25 @@
 module.exports = {
-    
-    incoming: function(message, args) {
+
+    incoming: function (message, args) {
         // already exists
         // create new obj
         // is call center / agent
         console.log(global.addressBook)
         console.log(args);
-        console.log(global.addressBook);
-        console.log('************');
-        global.newUser = new global.User(message);
-        global.userList.append(global.newUser)
-        console.log(global.newUser)
         console.log(global.userList);
+        global.userList.push(new global.User(message))
+        console.log(global.userList);
+        console.log('************');
+
 
         // message.text = 'This is a test';
         return message;
     },
-    outgoing: function(message, args) {
+    outgoing: function (message, args) {
         console.log(args);
         console.log('===========');
         message.text = 'Outgoing test';
-        return message 
+        return message
 
-    } 
+    }
 }
