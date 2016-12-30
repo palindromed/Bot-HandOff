c1c9eb042d9548f55916b71222d7713d13b21ab1
Hannah Krager <hakrager@microsoft.com>
2016-12-30 13:45:52 -0800

    fix error

--- globals.js
+++ globals.js
@@ -6,7 +6,7 @@ module.exports = function () {
         this.isAgent = args.user.isStaff || false,
         this.lastActivity = args.timestamp, // use to decide which conversations are 'current'. Let devs choose appropriate delta
         this.talkingToAgent = false, // may get rid of this. if alt addy can be an address or false, simple if can choose routing
-        routeMsgsTo = false // alternative to setting flag and changing address in user obj
+        this.routeMsgsTo = false // alternative to setting flag and changing address in user obj
     };
     global.Agent = function (args) {
         // keep track of convos part of
