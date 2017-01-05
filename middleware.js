80f50dc43697064a8fe152470ad4356575397266
Hannah Krager <hakrager@microsoft.com>
2017-01-05 10:26:26 -0800

    not adding to arrays or objs

--- middleware.js
+++ middleware.js
@@ -12,30 +12,32 @@ module.exports = {
         if (message.text === 'help') {
             // user initiated connect to agent
             // send something like 'hold on while I connect you'
-            global.conversations[message.conversation.id] =  Object.assign({}, global.conversations[message.conversation.id],  { findingAgent: true } );
+            console.log('user typed help');
+            global.conversations[message.conversation.id] = Object.assign({}, global.conversations[message.conversation.id], { findingAgent: true });
         };
 
         // find out who is talking / add new convo if not found
         if (message.user.isStaff) {
-            if (!global.agent.includes(message.conversation.id)) {
+            // if (!global.agent.includes(message.conversation.id)) {
 
-                global.agent.push(message.conversation.id);
-            };
-        global.conversations[message.conversation.id] = Object.assign({},global.conversations[message.conversation.id], { address: message.address} )
-            
-        } else if (!global.users.includes(message.conversation.id)) {
+            global.agent.push(message.conversation.id);
+            global.conversations[message.conversation.id] = Object.assign({}, global.conversations[message.conversation.id], { address: message.address })
+
+        } else {
 
             global.users.push(message.conversation.id);
 
         };
         if (!message.user.isStaff) {
-            global.conversations[message.conversation.id] = Object.assign({}, global.conversations[message.conversation.id], {  transcript: [message],
-                address: message.address} )
-            
-    // this needs to be an array of message objects and an indication of where messages should be routed
+            global.conversations[message.conversation.id] = Object.assign({}, global.conversations[message.conversation.id], {
+                transcript: [message],
+                address: message.address
+            })
+
+            // this needs to be an array of message objects and an indication of where messages should be routed
         }
         if (global.conversation[message.conversation.id].findingAgent) {
-            global.conversations[message.conversation.id] = Object.assign({}, global.conversations[message.conversation.id], {agentAddress: global.conversations[global.agent[0]].address, findingAgent: false});
+            global.conversations[message.conversation.id] = Object.assign({}, global.conversations[message.conversation.id], { agentAddress: global.conversations[global.agent[0]].address, findingAgent: false });
         }
 
 
