27ffd1d76b18e0222ae3851dc8ece1fac2b86a5e
Hannah Krager <hakrager@microsoft.com>
2017-01-05 16:32:25 -0800

    refactor a bit

--- middleware.js
+++ middleware.js
@@ -9,60 +9,67 @@ module.exports = {
         console.log('agents');
         console.log(global.agent);
 
-        if (message.text === 'help') {
-            // user initiated connect to agent
-            // send something like 'hold on while I connect you'
-            global.conversations[message.address.conversation.id] = Object.assign({}, global.conversations[message.address.conversation.id], { findingAgent: true });
-            message.text = 'hold on while I connect you';
-            return message
-        };
+        if (message.text) { // saves a ping from adding a user/agent
 
-        // find out who is talking / add new convo if not found
-        if (message.user.isStaff) {
-            console.log('is staff');
-            var thisAgent = global.agent.filter(function (value) {
-                return value === message.address.conversation.id;
-            })
-            if (thisAgent[0] !== message.address.conversation.id) {
-                global.agent.push(message.address.conversation.id);
-
+            if (message.text === 'help') {
+                // user initiated connect to agent
+                global.conversations[message.address.conversation.id] = Object.assign({}, global.conversations[message.address.conversation.id], { 'status': 'Finding_Agent' });
+                message.text = 'hold on while I connect you';
+                // return message
             };
 
-            global.conversations[message.address.conversation.id] = Object.assign({}, global.conversations[message.address.conversation.id], { address: message.address })
+            // find out who is talking / add new convo if not found
+            if (message.user.isStaff) { // move logic to an API for agents
+                console.log('is staff');
+                var agentId = message.address.conversation.id;
+                var thisAgent = global.conversations[agentId];
+                if (thisAgent === 'undefined') { // agent not in state yet, add them ****This will happen for each conversation, not agent.
+                    global.agent.push(agentId);
+                    global.conversations[agentId].address = message.address;
+                }
+            }
+            // End Agent
 
-        };
-        if (!message.user.isStaff) {
-            console.log('not staff');
-            var thisUser = global.users.filter(function (value) {
-                return value === message.address.conversation.id;
-            });
-            if (thisUser[0] !== message.address.conversation.id) {
-                global.users.push(message.address.conversation.id);
+            // Setting User state logic
+            else {
+                console.log('not staff');
+                var userId = message.address.conversation.id;
+                var thisUser = global.conversations[userId];
+                // Add a user not yet in state
+
+                if (thisUser === 'undefined') {
+                    global.users.push(userId);
+                    global.conversations[userId] = { transcript: [message], address: message.address, status: 'Talking_To_Bot' };
+                    return
+                }
+                // if in state, update transcript for the user
+                global.conversations[userId] = Object.assign({}, global.conversations[userId], { transcript: [...global.conversations[userId].transcript, message] });
+
+                // Check for choices to be made
+                switch (global.conversations[userId].status) {
+                    case 'Finding_Agent':
+                        console.log('getting agent');
+                        var myAgent = global.conversations[global.agent[0]];
+                        global.conversations[userId] = Object.assign({}, global.conversations[message.address.conversation.id], { agentAddress: myAgent.address, 'status': 'Talking_To_Agent' });
+                        break;
+                    case 'Talking_To_Agent':
+                        console.log('talking to agent');
+                        break;
+                    case 'Talking_To_Bot':
+                        console.log('talk to bot');
+                        break;
+                    default:
+                        break;
+
+                }
 
             }
-
-            global.conversations[message.address.conversation.id] = Object.assign({}, global.conversations[message.address.conversation.id], {
-                transcript: global.conversations[message.address.conversation.id].transcript.push(message),
-                address: message.address
-            })
-
-            // this needs to be an array of message objects and an indication of where messages should be routed
-        }
-        if (global.conversation[message.address.conversation.id].findingAgent) {
-            console.log('getting agent');
-            var myAgent = global.conversations[global.agent[0]].address;
-            global.conversations[message.address.conversation.id] = Object.assign({}, global.conversations[message.address.conversation.id], { agentAddress: myAgent, findingAgent: false })
-
         }
 
 
         // who should they be talking to
-
         // make it so
-
-
         // if talking to Agent/waiting for agent, suppress default bot functionality (how if on a prompt and it's looking for a certain response? override somehow)
 
     }
-
 }
