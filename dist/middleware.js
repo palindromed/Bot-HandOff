5f6349f652267d5921d7b8655ffa37827622313b
Hannah Krager <hakrager@microsoft.com>
2017-01-10 14:20:24 -0800

    add dist for azure, log incoming event

--- dist/middleware.js
+++ dist/middleware.js
@@ -1,79 +1,64 @@
-import * as builder from 'botbuilder';
-import { conversations, ConversationState, TranscriptLine } from './globals';
-
-
-const addToTranscript = (transcript: TranscriptLine[], message: builder.IMessage) => {
+"use strict";
+const builder = require("botbuilder");
+const globals_1 = require("./globals");
+const addToTranscript = (transcript, message) => {
     console.log("transcribing message", message);
     transcript.push({
         timestamp: message.timestamp,
         from: message.address,
         text: message.text
     });
-}
-
-export const route = (
-    event: builder.IEvent,
-    bot: builder.UniversalBot,
-    next: Function
-) => {
+};
+exports.route = (event, bot, next) => {
     console.log("middleware event", event);
-    console.log("conversations", conversations);
-
+    console.log("conversations", globals_1.conversations);
     switch (event.type) {
         case 'message':
-            const message = event as builder.IMessage;
+            const message = event;
             if (message.user.name.startsWith("Agent")) {
                 console.log("message from agent");
                 // If we're hearing from an agent they are already part of a conversation
-                const conversation = conversations.find(conversation =>
-                    conversation.agent.conversation.id === message.address.conversation.id
-                );
-
+                const conversation = globals_1.conversations.find(conversation => conversation.agent.conversation.id === message.address.conversation.id);
                 if (!conversation) {
                     bot.send(new builder.Message().address(message.address).text("You are no longer in conversation with the user"));
                     return;
                 }
-
-                if (conversation.state !== ConversationState.Agent) {
+                if (conversation.state !== globals_1.ConversationState.Agent) {
                     bot.send(new builder.Message().address(message.address).text("Shouldn't be in this state - agent should have been cleared out."));
                     console.log("Shouldn't be in this state - agent should have been cleared out");
                     return;
                 }
-
                 console.log("passing agent message to user");
                 addToTranscript(conversation.transcript, message);
                 bot.send(new builder.Message().address(conversation.customer).text(message.text));
-            } else {
+            }
+            else {
                 console.log("message from customer");
-                let conversation = conversations.find(conversation =>
-                    conversation.customer.conversation.id === message.address.conversation.id
-                );
-
+                let conversation = globals_1.conversations.find(conversation => conversation.customer.conversation.id === message.address.conversation.id);
                 if (!conversation) {
                     // first time caller, long time listener
                     conversation = {
                         customer: message.address,
-                        state: ConversationState.Bot,
+                        state: globals_1.ConversationState.Bot,
                         transcript: []
                     };
-                    conversations.push(conversation);
+                    globals_1.conversations.push(conversation);
                 }
                 addToTranscript(conversation.transcript, message);
-
                 switch (conversation.state) {
-                    case ConversationState.Bot:
+                    case globals_1.ConversationState.Bot:
                         if (message.text === 'help') {
                             console.log("switching to Waiting");
-                            conversation.state = ConversationState.Waiting;
+                            conversation.state = globals_1.ConversationState.Waiting;
                             return;
                         }
                         console.log("pasing message to bot");
                         return next();
-                    case ConversationState.Waiting:
+                    case globals_1.ConversationState.Waiting:
                         console.log("ignore message while waiting");
                         bot.send(new builder.Message().address(message.address).text("Connecting you to the next available agent."));
                         return;
-                    case ConversationState.Agent:
+                    case globals_1.ConversationState.Agent:
                         if (!conversation.agent) {
                             bot.send(new builder.Message().address(message.address).text("No agent address present while customer in state Agent"));
                             console.log("No agent address present while customer in state Agent");
@@ -83,9 +68,8 @@ export const route = (
                         bot.send(new builder.Message().address(conversation.agent).text(message.text));
                         return;
                 }
-
             }
-            case 'conversationUpdate':
-                console.log(event.user);
+        case 'conversationUpdate':
+            console.log(event.user);
     }
-}
+};
