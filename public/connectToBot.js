cfd4866a89397ffc5c7105af9874ed5bd3ec9cc7
Bill Barnes <billba@microsoft.com>
2017-01-20 12:26:42 -0800

    jQuery -> vanilla.js

--- public/connectToBot.js
+++ public/connectToBot.js
@@ -1,45 +1,42 @@
-$(function () {
-
-    $('#logon-form').on('submit', function (event) {
-        event.preventDefault();
-        $('#logon-form').hide();
-        var params = {};
-        location
-            .search
-            .substring(1)
-            .split("&")
-            .forEach(function (pair) {
-                var p = pair.split("=");
-                params[p[0]] = decodeURIComponent(p[1]);
-            });
-
-        var botConnection = new BotChat.DirectLine({
-            secret: params['s'],
-            token: params['t'],
-            domain: params['domain'],
-            webSocket: true
-        });
-
-
-        var user = {
-            id: $('#user-id').val(),
-            name: $('#user-id').val()
-        };
-
-        var bot = {
-            id: params['botid'] || 'botid',
-            name: params["botname"] || 'botname'
-        };
-
-        BotChat.App({
-            botConnection: botConnection,
-            user: user,
-            bot: bot,
-        }, document.getElementById("BotChatGoesHere"));
-
-
+var params = {};
+location
+    .search
+    .substring(1)
+    .split("&")
+    .forEach(function (pair) {
+        var p = pair.split("=");
+        params[p[0]] = decodeURIComponent(p[1]);
     });
 
-
-
+var botConnection = new BotChat.DirectLine({
+    secret: params['s'],
+    token: params['t'],
+    domain: params['domain'],
+    webSocket: true
 });
+
+var bot = {
+    id: params['botid'] || 'botid',
+    name: params["botname"] || 'botname'
+};
+
+var logon_form = document.getElementById('logon-form');
+var user_id = document.getElementById('user-id')
+
+logon_form.onsubmit = e => {
+    e.preventDefault();
+    logon_form.style.display = 'none';
+    console.log("e", e);
+
+    var user = {
+        id: user_id.value,
+        name: user_id.value
+    }
+
+    BotChat.App({
+        botConnection: botConnection,
+        user: user,
+        bot: bot,
+    }, document.getElementById("BotChatGoesHere"));
+}
+
