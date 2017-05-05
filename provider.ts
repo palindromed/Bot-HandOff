a44a8041fc20fd16854056d21570d77d014043cd
Christopher Harrison <charrison@geektrainer.com>
2017-05-05 16:28:53 -0400

    Updated to use async

--- provider.ts
+++ provider.ts
@@ -1,119 +1,123 @@
-// import * as builder from 'botbuilder';
-// import { Provider, Conversation, By, ConversationState } from './handoff';
+import * as builder from 'botbuilder';
+import { Provider, Conversation, By, ConversationState } from './handoff';
 
-// export let conversations: Conversation[];
+export let conversations: Conversation[];
 
-// export const init = () => {
-//     conversations = [];
-// }
+export const init = () => {
+    conversations = [];
+}
 
-// // Update
-// const addToTranscript = (by: By, text: string) => {
-//     const conversation = getConversation(by);
-//     if (!conversation)
-//         return false;
+const convertToPromise = <T>(value: T): Promise<T> => {
+    return new Promise<T>((resolve) => { return resolve(value) });
+}
 
-//     conversation.transcript.push({
-//         timestamp: Date.now(),
-//         from: by.agentConversationId ? 'agent' : 'customer',
-//         text
-//     });
+// Update
+const addToTranscript = async (by: By, text: string): Promise<boolean> => {
+    const conversation = await getConversation(by);
+    if (!conversation)
+        return convertToPromise<boolean>(false);
 
-//     return true;
-// }
+    conversation.transcript.push({
+        timestamp: Date.now(),
+        from: by.agentConversationId ? 'agent' : 'customer',
+        text
+    });
 
-// const connectCustomerToAgent = (by: By, agentAddress: builder.IAddress) => {
-//     const conversation = getConversation(by);
-//     if (conversation) {
-//         conversation.state = ConversationState.Agent;
-//         conversation.agent = agentAddress;
-//     }
+    return convertToPromise<boolean>(true);
+}
 
-//     return conversation;
-// }
+const connectCustomerToAgent = async (by: By, agentAddress: builder.IAddress): Promise<Conversation> => {
+    const conversation = await getConversation(by);
+    if (conversation) {
+        conversation.state = ConversationState.Agent;
+        conversation.agent = agentAddress;
+    }
 
-// const queueCustomerForAgent = (by: By) => {
-//     const conversation = getConversation(by);
-//     if (!conversation)
-//         return false;
+    return convertToPromise(conversation);
+}
 
-//     conversation.state = ConversationState.Waiting;
-//     if (conversation.agent)
-//         delete conversation.agent;
+const queueCustomerForAgent = async (by: By) => {
+    const conversation = await getConversation(by);
+    if (!conversation)
+        return convertToPromise(false);
 
-//     return true;
-// }
+    conversation.state = ConversationState.Waiting;
+    if (conversation.agent)
+        delete conversation.agent;
 
-// const connectCustomerToBot = (by: By) => {
-//     const conversation = getConversation(by);
-//     if (!conversation)
-//         return false;
+    return convertToPromise(true);
+}
 
-//     conversation.state = ConversationState.Bot;
-//     if (conversation.agent)
-//         delete conversation.agent;
+const connectCustomerToBot = async (by: By) => {
+    const conversation = await getConversation(by);
+    if (!conversation)
+        return convertToPromise(false);
 
-//     return true;
-// }
+    conversation.state = ConversationState.Bot;
+    if (conversation.agent)
+        delete conversation.agent;
 
-// // Get
-// const getConversation = (
-//     by: By,
-//     customerAddress?: builder.IAddress // if looking up by customerConversationId, create new conversation if one doesn't already exist
-// ) => {
-//     // local function to create a conversation if customer does not already have one
-//     const createConversation = (customerAddress: builder.IAddress) => {
-//         const conversation = {
-//             customer: customerAddress,
-//             state: ConversationState.Bot,
-//             transcript: []
-//         };
-//         conversations.push(conversation);
-//         return conversation;
-//     }
+    return convertToPromise(true);
+}
 
-//     if (by.bestChoice) {
-//         const waitingLongest = conversations
-//             .filter(conversation => conversation.state === ConversationState.Waiting)
-//             .sort((x, y) => y.transcript[y.transcript.length - 1].timestamp - x.transcript[x.transcript.length - 1].timestamp);
-//         return waitingLongest.length > 0 && waitingLongest[0];
-//     }
-//     if (by.customerName) {
-//         return conversations.find(conversation =>
-//             conversation.customer.user.name == by.customerName
-//         );
-//     } else if (by.agentConversationId) {
-//         return conversations.find(conversation =>
-//             conversation.agent && conversation.agent.conversation.id === by.agentConversationId
-//         );
-//     } else if (by.customerConversationId) {
-//         let conversation = conversations.find(conversation =>
-//             conversation.customer.conversation.id === by.customerConversationId
-//         );
-//         if (!conversation && customerAddress) {
-//             conversation = createConversation(customerAddress);
-//         }
-//         return conversation;
-//     }
-//     return null;
-// }
+// Get
+const getConversation = async (
+    by: By,
+    customerAddress?: builder.IAddress // if looking up by customerConversationId, create new conversation if one doesn't already exist
+): Promise<Conversation> => {
+    // local function to create a conversation if customer does not already have one
+    const createConversation = (customerAddress: builder.IAddress) => {
+        const conversation = {
+            customer: customerAddress,
+            state: ConversationState.Bot,
+            transcript: []
+        };
+        conversations.push(conversation);
+        return convertToPromise(conversation);
+    }
 
-// const currentConversations = () => 
-//     conversations;
+    if (by.bestChoice) {
+        const waitingLongest = conversations
+            .filter(conversation => conversation.state === ConversationState.Waiting)
+            .sort((x, y) => y.transcript[y.transcript.length - 1].timestamp - x.transcript[x.transcript.length - 1].timestamp);
+        return await convertToPromise(waitingLongest.length > 0 && waitingLongest[0]);
+    }
+    if (by.customerName) {
+        return convertToPromise(conversations.find(conversation =>
+            conversation.customer.user.name == by.customerName
+        ));
+    } else if (by.agentConversationId) {
+        return convertToPromise(conversations.find(conversation =>
+            conversation.agent && conversation.agent.conversation.id === by.agentConversationId
+        ));
+    } else if (by.customerConversationId) {
+        let conversation = conversations.find(conversation =>
+            conversation.customer.conversation.id === by.customerConversationId
+        );
+        if (!conversation && customerAddress) {
+            conversation = await createConversation(customerAddress);
+        }
+        return convertToPromise(conversation);
+    }
+    return null;
+}
 
-// export const defaultProvider: Provider = {
-//     init,
+const getCurrentConversations = (): Promise<Conversation[]> =>
+    convertToPromise(conversations);
 
-//     // Update
-//     addToTranscript,
-//     connectCustomerToAgent,
-//     connectCustomerToBot,
-//     queueCustomerForAgent,
+export const defaultProvider: Provider = {
+    init,
 
-//     // Get
-//     getConversation,
-//     currentConversations,
-// }
+    // Update
+    addToTranscript,
+    connectCustomerToAgent,
+    connectCustomerToBot,
+    queueCustomerForAgent,
+
+    // Get
+    getConversation,
+    getCurrentConversations,
+}
 
 
 
