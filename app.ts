7fbd2d87fb1365b9c7c47a1b46f6738e6208f944
Christopher Harrison <charrison@geektrainer.com>
2017-05-05 12:36:30 -0400

    format updates

--- app.ts
+++ app.ts
@@ -15,8 +15,8 @@ app.listen(process.env.port || process.env.PORT || 3978, '::', () => {
 });
 // Create chat bot
 const connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID || 'd959d5e8-0be6-4169-a361-2380793a21dd',
-    appPassword: process.env.MICROSOFT_APP_PASSWORD || 'dd1dPbTyJnDpBejPtuaJmU9'
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 const bot = new builder.UniversalBot(connector, [
