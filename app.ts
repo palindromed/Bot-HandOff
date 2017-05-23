c6a88933c5d667484fd621bd1c930287c26a0576
Lilian Kasem <likasem@Lilians-MacBook-Pro.local>
2017-05-22 17:35:27 -0700

    Adding Andre Price's work on the conversation api endpoint

--- app.ts
+++ app.ts
@@ -1,14 +1,16 @@
 import * as express from 'express';
 import * as builder from 'botbuilder';
+import * as bodyParser from 'body-parser';
 import { Handoff } from './handoff';
 import { commandsMiddleware } from './commands';
+import { MongooseProvider } from './mongoose-provider';
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
 const app = express();
-
+const mongooseProvider = new MongooseProvider();
 // Setup Express Server
 app.listen(process.env.port || process.env.PORT || 3978, '::', () => {
     console.log('Server Up');
@@ -25,16 +27,47 @@ const bot = new builder.UniversalBot(connector, [
     }
 ]);
 
+app.use(bodyParser.json());
+
 app.post('/api/messages', connector.listen());
+
+app.get('/api/conversations', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    console.log(req.headers);
+    if(authHeader) {
+        if(authHeader === 'Bearer ' + process.env.MICROSOFT_APP_PASSWORD) {
+            let conversations = await mongooseProvider.getCurrentConversations()
+            res.status(200).send(conversations);
+        }
+    }
+    res.status(401).send('Not Authorized');
+});
+
+app.post('/api/conversations', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    console.log(req.headers);
+    if(authHeader) {
+        if(authHeader === 'Bearer ' + process.env.MICROSOFT_APP_PASSWORD) {
+            if (await handoff.tweakConvo(req.body.conversationId)) {
+                res.status(200).send("Ok");
+            } else {
+                res.status(400).send("Meh");
+            }
+        }
+    }
+    res.status(401).send('Not Authorized');
+});
 
 // Create endpoint for agent / call center
 app.use('/webchat', express.static('public'));
 
 // replace this function with custom login/verification for agents
-const isAgent = (session: builder.Session) => 
+const isAgent = (session: builder.Session) =>
     session.message.user.name.startsWith("Agent");
 
-const isOperator = (session: builder.Session) => 
+const isOperator = (session: builder.Session) =>
     session.message.user.name.startsWith("Operator");
 
 const handoff = new Handoff(bot, isAgent, isOperator);
